f28eaa449a96fa7c247d2baad696f94323da0c4d
Lauren Coffman <lvcoffman@gmail.com>
2019-10-29 15:23:08 -0500

    Fixed encoding error in BOM source.  Changed code to work with new data source.

--- DataFrameBOMs.py
+++ DataFrameBOMs.py
@@ -2,21 +2,22 @@ import pandas as pd
 import numpy as np
 import os
 from Strip import strip_obj
-# pd.set_option('display.max_columns',20)
+pd.set_option('display.max_columns', 20)
+
+# Data source SAP t-code CS11 plant 90
 
 # create list of files to import
 folder = r"C:\Users\coffmlv\Documents\1_ESD\Inventory Lead Time\BOMs"
 filelist = os.listdir(folder)
 
 # call out specific column names to include (ignoring all others)
-colNames = ['Obj    ', 'Object description                      ', 'Quantity', 'Un']
+colNames = ['Component number', 'Object description', 'Comp. Qty (CUn)', 'Un']
 
 list_dfs = []
 # loop through and import each file as a dataframe in the list object
 for filename in filelist:
     path = os.path.join(folder, filename)
-    #print(path)
-    list_dfs.append(pd.read_csv(os.path.realpath(path), sep='|', skiprows=9, comment="-", usecols=colNames, encoding='unicode_escape').rename(columns=lambda x: x.strip()))
+    list_dfs.append(pd.read_csv(os.path.realpath(path), usecols=colNames))
 
 # add the file name as a new column to each dataframe
 for dataframe, filename in zip(list_dfs, filelist):
@@ -26,14 +27,14 @@ for dataframe, filename in zip(list_dfs, filelist):
 bom = pd.concat(list_dfs, ignore_index=True)
 bom = bom.apply(strip_obj, axis=0)
 
-NewColNames = {'Obj':'PartNumber', 'Object description':'PartDesc', 'Quantity':'BomQty', 'Un':'BOMunit'}
+NewColNames = {'Component number':'PartNumber', 'Object description':'PartDesc', 'Comp. Qty (CUn)':'BomQty', 'Un':'BOMunit'}
 
 bom = bom.rename(NewColNames, axis=1)
-bom['BomQty'] = pd.to_numeric(bom.BomQty)
-bom['BomQty'] = bom.BomQty.fillna(0.0)
+# bom['BomQty'] = pd.to_numeric(bom.BomQty)
+# bom['BomQty'] = bom.BomQty.fillna(0.0)
 
 
 # print(bom.columns)
 # print(bom.sample(20))
-# print(bom[bom['CatItem'].str.contains('USP-230AS50.88')])
+# print(bom[bom['PartNumber'].str.contains('PTS1419')])
 # print(bom['BomQty'].describe())
